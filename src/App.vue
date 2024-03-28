7ee6ac1788ae81a714c9e904414285ba46b31437
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-03-28 12:30:35 +0300

    work space

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <button @click = "addLike">Like</button>
+    <button @click = "addDisLike">DisLike</button>
+    <div>
+      Кол-во лайков <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      Кол-во дизлайков <strong>{{ dislikes }}</strong>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDisLike() {
+      this.dislikes += 1;
+    }
+  }
+}
+</script>
 
 <style>
 #app {
